8afe2cfb2a23a7faee152fc47b394b3c904a67a3
Fulton Wilcox III <robbiewilcox@nbp-12-48.nbp.ruw.rutgers.edu>
2023-02-02 09:27:20 -0500

    Min Heap implementation

---
+++ MinHeap.py
@@ -0,0 +1,38 @@
+import math
+class MinHeap:
+    def __init__(self):
+        self.arr = [0]
+
+    def addNum(self, num):
+        self.arr.append(num)
+        self.swim(len(self.arr)-1)
+        return
+
+    def pop(self):
+        num = self.arr[len(self.arr) - 1]
+        self.arr[len(self.arr) - 1] = self.arr[1]
+        self.arr[1] = num
+        returnNum = self.arr.pop(len(self.arr) - 1)
+        self.sink(1)
+        return returnNum
+
+    def sink(self, index):
+        while index*2 < len(self.arr) and self.arr[index] > self.arr[index*2]:
+            num = self.arr[index]
+            if index*2 + 1 >= len(self.arr) or self.arr[index*2] < self.arr[index*2 + 1]:
+                self.arr[index] = self.arr[index*2]
+                self.arr[index*2] = num
+                index = index*2
+            else: 
+                self.arr[index] = self.arr[index*2 + 1]
+                self.arr[index*2 + 1] = num
+                index = index*2 + 1
+        return
+
+    def swim(self, index):
+        while self.arr[index] < self.arr[math.floor(index/2)]:
+            num = self.arr[index]
+            self.arr[index] = self.arr[math.floor(index/2)]
+            self.arr[math.floor(index/2)] = num
+            index = math.floor(index/2)
+        return
--- Test.py
+++ Test.py
@@ -1,1 +1,15 @@
-print("Xin chao Long")
+from MinHeap import MinHeap
+
+##file used to test min heap, give it a try and make sure it works - Robbie
+
+x = MinHeap()
+x.addNum(1.76)
+x.addNum(3.245)
+x.addNum(4.1324)
+x.addNum(1.7)
+x.addNum(1.72)
+x.addNum(0.11)
+print(x.arr)
+while len(x.arr) > 1:
+    y = x.pop()
+    print("popped {}".format(y))
