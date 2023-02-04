62ed3892b4ad81a6f36386196a941d232a5b9c2c
Giyuu <tranhoanglong1904@gmail.com>
2023-02-04 13:58:43 -0500

    Define heap items to be ((x,y), f_value)

--- Test.py
+++ Test.py
@@ -6,3 +6,14 @@ import random
 ##file used to test min heap, give it a try and make sure it works - Robbie
 
 x = Maze()
+
+heap = MinHeap()
+heap.addNum((1,1), 3)
+heap.addNum((2,3), 2)
+heap.addNum((4,2), 9)
+heap.addNum((2,2), 5)
+print(heap.arr)
+
+while len(heap.arr) > 1:
+    y = heap.pop()
+    print("popped {} move".format(y[1]), y[0])
