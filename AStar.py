10fd1fb87aaa51981fa3132ad30e8d2863249a5e
Fulton Wilcox III <robbiewilcox@Fultons-MBP.fios-router.home>
2023-02-04 19:09:18 -0500

    A* annotations

--- AStar.py
+++ AStar.py
@@ -5,26 +5,27 @@ class AStar:
     
     @staticmethod
     def execute(initial, goal, maze, visited):
-        current = initial
-        heap = MinHeap()
-        heap.addNum(current, 0, initial)
+        current = initial  #current spot
+        heap = MinHeap() # heap for expansion
+        heap.addNum(current, 0, initial) #add current move to heap
         counter = 0
-        moves = []
+        moves = [] #array to backtrack when solution is found
         while(not heap.isEmpty()):
-            current = heap.pop() 
+            current = heap.pop() #pick least expensive move
             if current[0] == goal:
                 AStar.addPath(moves, maze, initial)
                 maze.print_maze()
                 return
-            moves.append(current)
-            visited[current[0][0]][current[0][1]] = 1
+            moves.append(current) #save move
+            visited[current[0][0]][current[0][1]] = 1 #mark as visited
             counter += 1
-            AStar.expand(visited, current, maze, heap, counter)
-        print("No solution bozo.")
+            AStar.expand(visited, current, maze, heap, counter) #expand from spot
+        print("No solution bozo.") #no solution if heap is empty
             
 
+    #looks complicated but all it does is add nodes that arent blocked to heap with f(n) and changes characters so they print in red
     @staticmethod
-    def expand(visited, current, maze, heap, counter):
+    def expand(visited, current, maze, heap, counter): 
         if current[0][0]-1 > 0:
             if visited[current[0][0]-1][current[0][1]] != 1 and maze.grid[current[0][0]-1][current[0][1]] != 1:
                 heap.addNum((current[0][0]-1, current[0][1]), maze.manhattans[current[0][0]-1][current[0][1]] + 1, (current[0][0], current[0][1]))
@@ -49,6 +50,7 @@ class AStar:
                 if maze.grid[current[0][0]][current[0][1]+1] != 3:
                     maze.grid[current[0][0]][current[0][1]+1] = '*'
 
+    #should print real shortest path in green, might be kind of off
     @staticmethod
     def addPath(moves, maze, initial):
         i = len(moves)-1
