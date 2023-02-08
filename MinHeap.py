62ef3dc49851bdba0b2086ff2dc6fbb58b13458a
Giyuu <tranhoanglong1904@gmail.com>
2023-02-07 19:52:28 -0500

    fixed minHeap sink

--- MinHeap.py
+++ MinHeap.py
@@ -2,11 +2,11 @@ import math
 from Node import Node
 class MinHeap:
     def __init__(self):
-        self.arr = [Node([0, 0], None, 0, 0)]
+        self.arr = [Node([-1, -1], None, -1, -1)]
 
     def addNode(self, node: Node):
         idx = self.find(node)
-        if idx:
+        if idx>0:
             self.update(idx,node)
         else: 
             self.arr.append(node)
@@ -23,20 +23,36 @@ class MinHeap:
 
     def sink(self, index):
         #print("-------------")
-        while index*2 < len(self.arr) and self.arr[index].total_cost > self.arr[index*2].total_cost:
+        
+        while index*2 < len(self.arr):
+            # num = self.arr[index]
+            # if index*2 + 1 >= len(self.arr):
+            #     self.arr[index] = self.arr[index*2]
+            #     self.arr[index*2] = num
+            #     index = index*2
+            #     continue
+            # if self.arr[index*2].total_cost < self.arr[index*2 + 1].total_cost:
+            #     self.arr[index] = self.arr[index*2]
+            #     self.arr[index*2] = num
+            #     index = index*2
+            # else:
+            #     self.arr[index] = self.arr[index*2 + 1]
+            #     self.arr[index*2 + 1] = num
+            #     index = index*2 + 1
+            child = index * 2 # child on left node
+            if (child + 1 < len(self.arr) and self.arr[child+1].total_cost < self.arr[child].total_cost):
+                child += 1 #choose child on right node
+            
+            if (self.arr[index].total_cost < self.arr[child].total_cost):
+                break
             num = self.arr[index]
-            if index*2 + 1 >= len(self.arr) or self.arr[index*2].total_cost < self.arr[index*2 + 1].total_cost:
-                self.arr[index] = self.arr[index*2]
-                self.arr[index*2] = num
-                index = index*2
-            else: 
-                self.arr[index] = self.arr[index*2 + 1]
-                self.arr[index*2 + 1] = num
-                index = index*2 + 1
+            self.arr[index] = self.arr[child]
+            self.arr[child] = num
+            index = child
         return
 
     def swim(self, index):
-        while index > 0 and self.arr[index].total_cost < self.arr[index//2].total_cost:
+        while self.arr[index].total_cost < self.arr[index//2].total_cost:
             num = self.arr[index]
             self.arr[index] = self.arr[index//2]
             self.arr[index//2] = num
@@ -50,7 +66,7 @@ class MinHeap:
        for i in range(len(self.arr)):
            if node.position == self.arr[i].position:
                return i
-       return False
+       return -1
     
     def update(self, index, node):
         if node.total_cost < self.arr[index].total_cost:
--- Test.py
+++ Test.py
@@ -19,8 +19,8 @@ for i in range(x.height):
 ## Test AStar
 
 
-# AStar.execute([x.startX, x.startY], [x.targetX, x.targetY], x, visited)
-heap = MinHeap()
+AStar.execute([x.startX, x.startY], [x.targetX, x.targetY], x, visited)
+# heap = MinHeap()
 # heap.addNode(Node((0,0), None, 2, 0))
 # heap.addNode(Node((0,2), None, 7, 0))
 # heap.addNode(Node((1,2), None, 4, 0))
@@ -29,16 +29,20 @@ heap = MinHeap()
 # heap.addNode(Node((7,2), None, 3, 0))
 # heap.addNode(Node((2,4), None, 5, 0))
 
-heap.addNode(Node((0,0), None, 8, 0))
-heap.addNode(Node((0,2), None, 7, 0))
-heap.addNode(Node((1,2), None, 6, 0))
-heap.addNode(Node((4,2), None, 5, 0))
-heap.addNode(Node((2,2), None, 4, 0))
-heap.addNode(Node((7,2), None, 3, 0))
-heap.addNode(Node((2,4), None, 2, 0))
+# heap.addNode(Node((0,0), None, 8, 0))
+# heap.addNode(Node((0,2), None, 7, 0))
+# heap.addNode(Node((1,2), None, 6, 0))
+# heap.addNode(Node((4,2), None, 5, 0))
+# heap.addNode(Node((2,2), None, 4, 0))
+# heap.addNode(Node((7,2), None, 3, 0))
+# heap.addNode(Node((2,4), None, 2, 0))
 
-print("[")
-for i in range(1, len(heap.arr)):
-    print(heap.arr[i].total_cost)
-print("]")
-#print([0, 1] == [0, 1])
+# print("[")
+# for i in range(1, len(heap.arr)):
+#     print(heap.arr[i].total_cost)
+# print("]")
+
+# print(heap.pop().total_cost)
+# print(heap.pop().total_cost)
+# print(heap.pop().total_cost)
+# print(heap.pop().total_cost)
